c46e8c7840d70b86746ebe1d76384893638d8bbc
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2021-10-14 01:10:47 +0200

    Initial commit
    - Hugo config and theme
    - Makefile and emacs init for ox-hugo
    - Gitlab CI

---
+++ emacs/init.el
@@ -0,0 +1,71 @@
+;;; build.el --- Minimal emacs installation to build the website -*- lexical-binding: t -*-
+;; Authors: Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
+;; Modified from: https://github.com/bphenriques/knowledge-base/blob/master/tools/init.el
+
+;;; Commentary:
+;;; - Requires BASE_DIR environment variable
+;;
+;; Notes:
+;; * This is not a package.
+;;; Code:
+
+(require 'subr-x)
+
+(toggle-debug-on-error)                                       ;; Show debug informaton as soon as error occurs.
+(setq
+ make-backup-files nil                                        ;; Disable "<file>~" backups.
+)
+
+(defconst base-dir
+  (let* ((env-key "BASE_DIR")
+         (env-value (getenv env-key)))
+    (if (and env-value (file-directory-p env-value))
+        env-value
+      (error (format "%s is not set or is not an existing directory (%s)" env-key env-value)))))
+
+;;;
+;;; Setup packages using straight.el: https://github.com/raxod502/straight.el
+;;;
+;;; Leads to better reproducible builds as the versions are pinned.
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-use-package-by-default t)
+(straight-use-package 'use-package)
+
+;;;
+;;; Setup packages
+;;;
+
+(use-package ox-hugo
+  :straight (:type git :host github :repo "kaushalmodi/ox-hugo" :nonrecursive t))
+
+(setf org-hugo-base-dir base-dir)
+
+;;;
+;;; Public functions
+;;;
+
+(defun build/export-all ()
+  "Export all org-files (including nested) under base-org-files."
+  (let ((search-path (file-name-as-directory base-dir)))
+    (message (format "[build] Looking for files at %s" search-path))
+    (dolist (org-file (directory-files-recursively search-path "(?<!emacs).*\.org$"))
+      (with-current-buffer (find-file org-file)
+   (message (format "[build] Exporting %s" org-file))
+   (org-hugo-export-wim-to-md :all-subtrees nil nil nil)))
+    (message "Done!")))
+
+(provide 'build/export-all)
+
+;;; init.el ends here
---
+++ makefile
@@ -0,0 +1,40 @@
+# Based on https://github.com/bphenriques/knowledge-base/blob/master/Makefile
+
+BASE_DIR := $(shell pwd)
+EMACS_BUILD_SRC := $(BASE_DIR)/emacs
+BASE_URL := https://research.curiouscoding.nl
+
+all: clean build-content serve
+
+.PHONY: clean
+clean:
+	rm -rf content public
+	rm -rf static/ox-hugo
+
+.PHONY: serve
+serve:
+	hugo server --minify --disableFastRender --navigateToChanged --baseURL localhost:1313
+
+.PHONY: build-content
+build-content:
+# Build temporary minimal EMACS installation separate from the one in the machine.
+	HOME=$(EMACS_BUILD_SRC) BASE_DIR=$(BASE_DIR) emacs -Q --batch --load $(EMACS_BUILD_SRC)/init.el --execute "(build/export-all)" --kill
+# Fixes bad URLs when the baseURL (this case) is not the root URL.
+# There is the option to change the template but I think the solution is too intrusive and it should pass through
+# changing the layout as it is seems to be a bug within HUGO.
+# https://github.com/kaushalmodi/ox-hugo/issues/460
+#find $(BASE_DIR)/content -type f -exec sed -i '' -e 's|figure src="/ox-hugo/|figure src="ox-hugo/|g' {} \;
+
+.PHONY: build-site
+build-site:
+	hugo --minify --cleanDestinationDir --baseURL $(BASE_URL)
+
+.PHONY: build
+build: build-content build-site
+
+.PHONY: deploy
+deploy: build-content build-site
+
+update-sub-modules:
+	git submodule update --init --recursive
+	git submodule foreach git pull origin master
