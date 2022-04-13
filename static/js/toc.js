5be92c243e79fa6be19575eb881764a40a0c2a24
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-04-13 16:24:05 +0200

    Add TOC

---
+++ static/js/toc.js
@@ -0,0 +1,51 @@
+let observer = new IntersectionObserver(handler, {
+  threshold: [0],
+});
+let paragraphs = [...document.querySelectorAll("article > div > *")];
+let submenu = [...document.querySelectorAll(".toc a")];
+function previousHeaderId(p) {
+  while (p && !p.matches("h2, h3, h4")) {
+    p = p.previousElementSibling;
+  }
+  return p?.id;
+}
+let paragraphMenuMap = paragraphs.reduce((acc, p) => {
+  let h = previousHeaderId(p);
+  p.previousHeader = h;
+  if (h) {
+    let entry = submenu.find((a) => a.hash === "#" + h);
+    acc[h] = entry;
+  }
+  return acc;
+}, {});
+
+paragraphs.forEach((elem) => observer.observe(elem));
+
+let selection;
+function handler(updates) {
+  selection = (selection || updates).map(
+    (s) => updates.find((e) => e.target === s.target) || s
+  );
+  console.log("update");
+  // Clear all.
+  for (s of selection) {
+    if (!s.isIntersecting) {
+      paragraphMenuMap[s.target.previousHeader]?.parentElement.classList.remove(
+        "selected",
+        "parent"
+      );
+    }
+  }
+  // Set all.
+  for (s of selection) {
+    if (s.isIntersecting) {
+      let parentElem = paragraphMenuMap[s.target.previousHeader]?.closest("li");
+      // ensure that parent menu entries are selected too
+      parentElem?.classList.add("selected");
+      while (parentElem) {
+        parentElem?.classList.add("parent");
+        parentElem = parentElem.parentElement.closest("li");
+      }
+    }
+  }
+}
