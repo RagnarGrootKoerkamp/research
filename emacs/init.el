ca6141ecd741c4c60eae180477624318af9ed2a8
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2022-10-28 15:04:19 +0200

    Add references.bib file

--- emacs/init.el
+++ emacs/init.el
@@ -69,6 +69,9 @@
 ; Use CSL by default
 (setq org-cite-export-processors '((t csl)))
 
+(setq references-file (expand-file-name "references.bib"))
+(setq org-cite-global-bibliography (list references-file))
+
 ;;;
 ;;; Public functions
 ;;;
