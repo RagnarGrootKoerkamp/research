0b111b26ad2eb4c310b47b3f907e8362d0e3a2f7
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-07-19 03:41:57 +0200

    specialblocks: make sure org-special-blocks-extras-mode is active

--- emacs/init.el
+++ emacs/init.el
@@ -78,9 +78,9 @@
 ;;; org-special-blocks-extra
 ;;;
 (use-package org-special-block-extras
-  ;; :ensure t
-  ;; Do not load a bunch of bloat.
-  ;; :hook (org-mode . org-special-block-extras-mode)
+  :ensure t
+  ;; Loads a bunch of bloat, but required to make the things below work.
+  :hook (org-mode . org-special-block-extras-mode)
   )
 
 (org-mode)
